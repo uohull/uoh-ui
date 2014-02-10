e7135677bee371fd839cb16fe219a7e020a8d4d9
richgj700 <rich.garbutt@gmail.com>
2014-02-10 09:54:19 +0000

    Initial empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UohUi::Application.config.secret_key_base = '0e1f521dd09c009db4324486304527951e7104ad8100b2f944b885c8e37b15f2f33591b84a281f95d9520b3d1c8f79e8b908d097d4e804a0037de4649a6fbd47'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UohUi::Application.config.session_store :cookie_store, key: '_uoh-ui_session'
